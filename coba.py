9274e8f785e7ee6c621b4841004e0f8be1bb8ec8
felixskdeleton <naufalzahran1979@gmail.com>
2024-11-07 13:54:58 +0700

    Menambah ucapan selamat

--- coba.py
+++ coba.py
@@ -1,5 +1,7 @@
-print("Hello Word")
+print("Hello World")
 print("Selamat Siang")
 print("Selamat Pagi")
 print("Selamat Sore")
 print("Selamat Malam")
+print("Selamat Makan")
+print("Selamat Tidur")
